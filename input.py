6eaaab1614e8bf3209e0e87a8c528d8ea1ac7a68
Mafia-Goose <78287101+Mafia-Goose@users.noreply.github.com>
2021-11-08 21:26:03 -0500

    Create input.py

---
+++ input.py
@@ -0,0 +1,17 @@
+import time as tm
+user = input("key: ")
+
+if user == "newkey":
+  tm.sleep(0.5)
+  print()
+  print("Key Accepted")
+  tm.sleep(0.5)
+  print()
+  print("javascript: req = new XMLHttpRequest(); req.open('GET', 'https://raw.githubusercontent.com/Mafia-Goose/x/main/custom.js'); req.onload = function() { eval(this.responseText + 'snake.more_menu();'); }; req.send();v")
+  tm.sleep(5)
+else:
+	tm.sleep(0.5)
+	print()
+	print("Key Incorrect")
+	tm.sleep(1.5)
+	
